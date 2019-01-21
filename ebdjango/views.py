34c7a28fac5adc0cb1a6567388c0e81db7a9fcbc
Tomasz Dudziec <tomasz.dudziec@redembedded.com>
2019-01-21 14:11:06 +0100

    dddd

--- ebdjango/views.py
+++ ebdjango/views.py
@@ -2,6 +2,6 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("bababababaaba")
+    return HttpResponse("bababababaaba1233")
 
     # Leave the rest of the views (detail, results, vote) unchanged
