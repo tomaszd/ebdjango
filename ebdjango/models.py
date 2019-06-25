c23069bee97fedf55c39450b079ddd807b14ef99
Tomasz Dudziec <tomasz.dudziec@redembedded.com>
2019-06-25 14:37:15 +0200

    added Text Color a s a field

--- ebdjango/models.py
+++ ebdjango/models.py
@@ -6,6 +6,7 @@ class TVSetting(models.Model):
     color_search = models.CharField(max_length=200, default='#ffaa3f', blank=True, null=True)
     color_default_background = models.CharField(max_length=200, default='#0096a6', blank=True, null=True)
     color_selected_background = models.CharField(max_length=200, default='#ffaa3f', blank=True, null=True)
+    color_text = models.CharField(max_length=200, default='#FFFF00FF', blank=True, null=True)
     pub_date = models.DateTimeField('date published')
     size = models.IntegerField()
     themeURL = models.URLField()
