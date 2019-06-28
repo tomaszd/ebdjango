c4907c0adbe39d7c54342a89e83068bc9d097fa7
Tomasz Dudziec <tomasz.dudziec@redembedded.com>
2019-06-28 16:23:41 +0200

    added support for card filtering

--- static/ebdjango/js/utils.js
+++ static/ebdjango/js/utils.js
@@ -19,3 +19,18 @@ function myFunctionCount() {
 function myFunctionStopCount() {
           document.getElementById("demo").innerHTML = "STOP counting";
 }
+
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
