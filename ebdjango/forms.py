35b7f373dc740bdc5076f3828f875cbd562da6ec
Tomasz Dudziec <tomasz.dudziec@redembedded.com>
2019-07-09 11:41:55 +0200

    added Snooker Results

--- ebdjango/forms.py
+++ ebdjango/forms.py
@@ -6,7 +6,7 @@ from .models import MatchResult
 class MatchResultForm(forms.ModelForm):
     class Meta:
         model = MatchResult
-        fields = ('player1', 'player2', 'setWonPlayer1', 'setWonPlayer2', 'finished', 'tournament_name','game_type')
+        fields = ('player1', 'player2', 'setWonPlayer1', 'setWonPlayer2', 'finished', 'tournament_name', 'game_type')
         labels = {
             'player1': "Player 1",
             'player2': "Player 2",
