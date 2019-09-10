f3533dec74f51d87a5c234e280204717a5387cee
Tomasz Dudziec <tomasz.dudziec@redembedded.com>
2019-09-10 12:48:44 +0200

    added the django-heroku

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import django_heroku
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
@@ -130,3 +132,4 @@ REST_FRAMEWORK = {
     ]
 }
 
+django_heroku.settings(locals())
